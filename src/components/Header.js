52982a39cfd5ccb84c6e8a400fdd3aa17799a081
BimbolaA <abimbolaabiri@gmail.com>
2022-10-28 02:03:39 +0100

    The Head Section

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+    return(
+        <header>
+            <img src='../images/me.jpg' alt='' id='profile__img' />
+            <p>Abimbola Abiri</p>
+        </header>
+    )
+}
