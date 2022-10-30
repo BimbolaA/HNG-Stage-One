0af49217cb6d559423f1562253a24b862dc85b1a
BimbolaA <abimbolaabiri@gmail.com>
2022-10-30 19:24:46 +0100

    Links styled like buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Header from './components/Header';
+import Buttons from './components/Buttons';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Buttons/>
     </div>
   );
 }
---
+++ src/components/Buttons.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Buttons() {
+    return(
+        <section>
+            <a href='' id='twitter'>Twitter Link</a>
+            <a href='' id='slack'>BimbolaA</a>
+            <a href='https://training.zuri.team/' id='btn__zuri'>BimbolaA</a>
+            <a href='http://books.zuri.team' id='books'>Design & Coding Books</a>
+            <a href='https://books.zuri.team/python-for-beginners?ref_id=BimbolaA' id='book__python'>Buy Books That Teach Python</a>
+            <a href='https://background.zuri.team' id='pitch'>Get Value for Money</a>
+            <a href='https://books.zuri.team/design-rules' id='book__design'>Learn More Pay Nothing</a>
+        </section>
+    );
+}
