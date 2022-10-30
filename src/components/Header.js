e3451586f8cde615370e1472678e4121ea20bbee
BimbolaA <abimbolaabiri@gmail.com>
2022-10-30 22:20:59 +0100

    Responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,10 @@ import React from 'react'
 export default function Header() {
     return(
         <header>
+            <div>
+                <img src='../images/mobile-share.png' alt='' id='icon_mobile'/>
+                <img src='../images/_Avatar share button.png' alt='' id='icon_top'/>
+            </div>
             <img src='../images/me.jpg' alt='' id='profile__img' />
             <p>Abimbola Abiri</p>
         </header>
